930e9bc207a4d9be97fbe538992fe88c7aa7e908
morgmaze <>
2024-04-30 21:15:33 +0100

    When you record a watering, save a future watering for that plant in 5 days.

---
+++ src/main/java/api/FutureWateringController.java
@@ -0,0 +1,33 @@
+package api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import model.FutureWatering;
+import persistence.FutureWateringRepository;
+
+@Controller
+public class FutureWateringController {
+	
+	@Autowired
+	FutureWateringRepository futureWateringRepository;
+	
+	
+	@GetMapping("/futureWaterings")
+	public String getFutureWaterings(Model model) {
+		List<FutureWatering> futureWaterings = (List<FutureWatering>) futureWateringRepository.findAll();
+		model.addAttribute("futureWaterings", futureWaterings);
+		
+		System.out.println("Future waterings:");
+		for (FutureWatering f : futureWaterings) {
+			System.out.println(f.toString());
+		}
+		
+		return "future-waterings";
+	}
+
+}
