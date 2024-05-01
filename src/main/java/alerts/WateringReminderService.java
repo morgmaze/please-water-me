3d226cd6e11d4e7397463d1c7db740b67d44f6ae
morgmaze <>
2024-05-01 11:16:30 +0100

    Send reminder emails when a plant is due for watering

--- src/main/java/alerts/WateringReminderService.java
+++ src/main/java/alerts/WateringReminderService.java
@@ -2,12 +2,15 @@ package alerts;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import app.Constants;
 import model.FutureWatering;
 import persistence.FutureWateringRepository;
 
@@ -21,15 +24,39 @@ public class WateringReminderService {
 	
 	// for testing: run every minute "0 * * * * *"
 	// run every day at 5 am "0 0 5 * * *"
-	@Scheduled(cron = "0 * * * * *") 
+	@Scheduled(cron = "0 * * * * *")
 	public void wateringAlertPoller() {
 		log.info("Checking database for today's watering reminders");
 		
 		// check the FutureWatering repository for reminder dates of today
 		List<FutureWatering> todaysReminders = futureWateringRepository.findFutureWateringsForToday();
 		
-		// TODO: send alerts
+		// send alerts
+		for (FutureWatering reminder : todaysReminders) {
+			sendWateringReminder(reminder);
+		}
+	}
+	
+	public void sendWateringReminder(FutureWatering reminder) {
+		log.info("Sending watering reminder: " + reminder.toString());
 		
+		String subject = "Please Water Me reminder!";
+		String body = constructEmailBody(reminder);
+		
+		try {
+        	GmailUtil.sendEmail(Constants.TO_EMAIL, Constants.FROM_EMAIL, subject, body, false);
+        } catch (MessagingException e) {
+        	System.out.println("Unable to send email");
+        	e.printStackTrace();
+        }
+	}
+	
+	private String constructEmailBody(FutureWatering reminder) {
+		String body = "This is a reminder that your " + reminder.getPlant().getSpecies() + " located in "
+				+ reminder.getPlant().getLocation() + " is due for watering today, " + reminder.getReminderDate()
+				+ ". Once you've watered it, be sure to log the watering in the Please Water Me application!";
+		
+		return body;
 	}
 
 }
---
+++ src/main/java/app/Constants.java
@@ -0,0 +1,21 @@
+package app;
+
+public final class Constants {
+
+	private Constants() {
+		
+	}
+	
+	// the user's email
+	public static final String TO_EMAIL = "morgan.mazer@gmail.com";
+	
+	// from email address
+	public static final String FROM_EMAIL = "mmazer3.jhu@gmail.com";
+	
+	// FROM_EMAIL app password
+	public static final String PASSWORD = "juricpsyajivlghz";
+	
+	// TODO: this will be customizable later
+	public static final int REMINDER_TIMEFRAME = 5;
+	
+}
