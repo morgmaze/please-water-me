d4bebe7e0c243a81284d6a49b77e02f6418e88d4
morgmaze <>
2024-05-02 11:20:52 +0100

    Initial plant care information feature. Store information for a few species, and added endpoint to retrieve and display information for a given species

--- src/main/java/config/DatabaseConfiguration.java
+++ src/main/java/config/DatabaseConfiguration.java
@@ -13,7 +13,9 @@ import org.springframework.context.annotation.Configuration;
 
 import model.FutureWatering;
 import model.Plant;
+import model.PlantSpeciesCareInformation;
 import persistence.FutureWateringRepository;
+import persistence.PlantCareDatabase;
 import persistence.PlantRepository;
 
 @Configuration
@@ -22,22 +24,34 @@ public class DatabaseConfiguration {
 	private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
 	@Bean
-	public CommandLineRunner initDatabase(PlantRepository plantRepository, FutureWateringRepository futureWateringRepository) {
+	public CommandLineRunner initDatabase(PlantRepository plantRepository,
+			FutureWateringRepository futureWateringRepository, PlantCareDatabase plantCareDatabase) {
 		log.info("Preloading data");
 		
 		return args -> {
 			// plant 1
-			Plant plant1 = new Plant("Monstera Deliciosa", "Guest Room", null);
+			Plant plant1 = new Plant("Monstera deliciosa", "Guest Room", null);
 			plant1 = plantRepository.save(plant1);
 			
 			futureWateringRepository.save(new FutureWatering(plant1, new Date()));
 			
 			// plant 2
 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-			Plant plant2 = new Plant("Snake Plant", "Dining Room", df.parse("2023-09-30"));
+			Plant plant2 = new Plant("Snake plant", "Dining Room", df.parse("2023-09-30"));
 			plant2 = plantRepository.save(plant2);
 			futureWateringRepository.save(new FutureWatering(plant2, df.parse("2024-05-15")));
 			
+			// populate plant_care_info database
+			plantCareDatabase.save(new PlantSpeciesCareInformation("Monstera deliciosa", "Swiss cheese plant", 7, 14,
+					"Medium to bright indrect", 65, 85, 18, 30, false, false));
+			plantCareDatabase.save(new PlantSpeciesCareInformation("Snake plant", "N/A", 14, 56,
+					"Partial sun", 60, 75, 16, 24, false, false));
+			plantCareDatabase.save(new PlantSpeciesCareInformation("Peace lily", "N/A", 7, 14,
+					"Medium to bright indirect", 65, 80, 18, 27, false, false));
+			plantCareDatabase.save(new PlantSpeciesCareInformation("Aloe vera", "N/A", 7, 10,
+					"Bright indirect", 55, 85, 13, 29, false, false));
+			plantCareDatabase.save(new PlantSpeciesCareInformation("Spider plant", "N/A", 7, 10,
+					"Bright indirect", 55, 80, 13, 27, true, true));
 		};
 	}
 	
---
+++ src/main/java/model/PlantSpeciesCareInformation.java
@@ -0,0 +1,166 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table(name = "plant_care_info")
+public class PlantSpeciesCareInformation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String species;
+
+	private String commonName;
+
+	private int minDaysToWater;
+
+	private int maxDaysToWater;
+
+	private String lightLevel;
+
+	private int minTemperatureF;
+
+	private int maxTemperatureF;
+
+	private int minTemperatureC;
+
+	private int maxTemperatureC;
+
+	private boolean safeForDogs;
+
+	private boolean safeForCats;
+	
+	
+	public PlantSpeciesCareInformation(String species, String commonName, int minDaysToWater, int maxDaysToWater,
+			String lightLevel, int minTemperatureF, int maxTemperatureF, int minTemperatureC, int maxTemperatureC,
+			boolean safeForDogs, boolean safeForCats) {
+		super();
+		this.species = species;
+		this.commonName = commonName;
+		this.minDaysToWater = minDaysToWater;
+		this.maxDaysToWater = maxDaysToWater;
+		this.lightLevel = lightLevel;
+		this.minTemperatureF = minTemperatureF;
+		this.maxTemperatureF = maxTemperatureF;
+		this.minTemperatureC = minTemperatureC;
+		this.maxTemperatureC = maxTemperatureC;
+		this.safeForDogs = safeForDogs;
+		this.safeForCats = safeForCats;
+	}
+
+	public PlantSpeciesCareInformation() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getCommonName() {
+		return commonName;
+	}
+
+	public void setCommonName(String commonName) {
+		this.commonName = commonName;
+	}
+
+	public int getMinDaysToWater() {
+		return minDaysToWater;
+	}
+
+	public void setMinDaysToWater(int minDaysToWater) {
+		this.minDaysToWater = minDaysToWater;
+	}
+
+	public int getMaxDaysToWater() {
+		return maxDaysToWater;
+	}
+
+	public void setMaxDaysToWater(int maxDaysToWater) {
+		this.maxDaysToWater = maxDaysToWater;
+	}
+
+	public String getLightLevel() {
+		return lightLevel;
+	}
+
+	public void setLightLevel(String lightLevel) {
+		this.lightLevel = lightLevel;
+	}
+
+	public int getMinTemperatureF() {
+		return minTemperatureF;
+	}
+
+	public void setMinTemperatureF(int minTemperatureF) {
+		this.minTemperatureF = minTemperatureF;
+	}
+
+	public int getMaxTemperatureF() {
+		return maxTemperatureF;
+	}
+
+	public void setMaxTemperatureF(int maxTemperatureF) {
+		this.maxTemperatureF = maxTemperatureF;
+	}
+
+	public int getMinTemperatureC() {
+		return minTemperatureC;
+	}
+
+	public void setMinTemperatureC(int minTemperatureC) {
+		this.minTemperatureC = minTemperatureC;
+	}
+
+	public int getMaxTemperatureC() {
+		return maxTemperatureC;
+	}
+
+	public void setMaxTemperatureC(int maxTemperatureC) {
+		this.maxTemperatureC = maxTemperatureC;
+	}
+
+	public boolean isSafeForDogs() {
+		return safeForDogs;
+	}
+
+	public void setSafeForDogs(boolean safeForDogs) {
+		this.safeForDogs = safeForDogs;
+	}
+
+	public boolean isSafeForCats() {
+		return safeForCats;
+	}
+
+	public void setSafeForCats(boolean safeForCats) {
+		this.safeForCats = safeForCats;
+	}
+
+	@Override
+	public String toString() {
+		return "PlantSpeciesCareInformation [id=" + id + ", species=" + species + ", commonName=" + commonName
+				+ ", minDaysToWater=" + minDaysToWater + ", maxDaysToWater=" + maxDaysToWater + ", lightLevel="
+				+ lightLevel + ", minTemperatureF=" + minTemperatureF + ", maxTemperatureF=" + maxTemperatureF
+				+ ", minTemperatureC=" + minTemperatureC + ", maxTemperatureC=" + maxTemperatureC + ", safeForDogs="
+				+ safeForDogs + ", safeForCats=" + safeForCats + "]";
+	}
+
+}
